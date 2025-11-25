769f4f328666369677ed3ec766db5cd06e2d0301
egolyub <tjesonx@gmail.com>
2025-11-25 20:21:11 +0800

    Inventory Update
    WW, пока работает с багами, предмет из инвентаря выпадает:
    если персонаж на земле
       то предмет падает в землю
    если персонаж в воздухе
    то предмет остается в воздухе
    Это происходит тк предметы не являются объектами, а просто картинка с параметрами

---
+++ My project/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public bool[] isFull;
+    public GameObject[] slots;
+    public GameObject inventory;
+    private bool inventoryOn;
+
+    private void Start()
+    {
+        inventory.SetActive(false);
+        inventoryOn = false;
+    }
+
+    public void Chest()
+    {
+        if(inventoryOn == false)
+        {
+            inventoryOn = true;
+            inventory.SetActive(true);
+        }
+        else if (inventoryOn == true)
+        {
+            inventoryOn = false;
+            inventory.SetActive(false);
+        }
+    }
+}
